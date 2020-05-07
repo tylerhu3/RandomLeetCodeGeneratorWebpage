5be87a7dfc062083dc3cd7e2bb4e5f67021213fc
tylerhu3 <tylerhu@yahoo.com>
2020-05-06 22:29:11 -0700

    we are now able to grab description of exactly 1 problem

--- webScrapLeet/grabAllQuestionDetailFromLeetcode.py
+++ webScrapLeet/grabAllQuestionDetailFromLeetcode.py
@@ -4,66 +4,26 @@ from urllib.request import urlopen, Request
 # from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 #https://stackoverflow.com/questions/45448994/wait-page-to-load-before-getting-data-with-requests-get-in-python-3
-reg_url = "http://leetcode.com/problems/find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/"
+"""
+For the below to work we need chromium driver from : https://sites.google.com/a/chromium.org/chromedriver/downloads
+download that and put chromedriver.exe into 
+...\Python\Python38-32\Scripts
+"""
 
-headers={
-    "User-Agent": "Chrome/80.0.3987.163",
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',} 
-req = Request(url=reg_url, headers=headers) 
-html = urlopen(req).read() 
+from bs4 import BeautifulSoup
+from selenium import webdriver
 
-print(html)
+# import selenium as se
 
-# user_agent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-# print(containers)
-# myUrl = "https://leetcode.com/problems/find-the-minimum-number-of-fibonacci-numbers-whose-sum-is-k/"
-# hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-#        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-#        'Accept-Encoding': 'none',
-#        'Accept-Language': 'en-US,en;q=0.8',
-#        'Connection': 'keep-alive'}
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
 
+driver = webdriver.Chrome(options=options)
 
-
-
-# f = codecs.open("x.html", 'r')
-# x0 = f.read()
-# print(x0)
-
-# page_soup = soup(x0, "html.parser")
-
-# class Leet:
-#     # class variable shared by all instances
-#     def __init__(self, num, title, url, diff):
-#         self.num = num
-#         self.title = title
-#         self.url = url
-#         self.diff = diff
-
-
-# containers = page_soup.findAll("tr") 
-
-# filename = "LeetQuestions.txt" #name of file
-# f = open(filename, "w") #open file and declare access type
-# headers = "#, Title, Sub-URL, Difficulty \n" # the headers for each column
-
-# f.write("[")
-
-# for container in containers:
-    # f.write("{")
-    # x1 = container.findAll("td")
-    #Please notice the "." gets the html tag and the brackets refer
-    #to the element within the tag. Also to get the text within the
-    #tags we do .text as the last parameter
-    # problemNumber = "\"num\": \"" + x1[1].text + "\""
-    # problemTitle = x1[2]["value"]
-    # problemURL = "https://leetcode.com" + x1[2].a["href"]
-    # problemDifficulty = "\"diff\": \"" + x1[5].span.text + "\""
-    # f.write( problemNumber + ",\n" + prooblemTitle.replace(",", "|") + ",\n" + problemURL + ",\n" + problemDifficulty +  "\n")
-    # print(problemURL)
-    # f.write("},\n")
-# f.write("]\n")
-
-# f.close()
-    
+url = "https://leetcode.com/problems/longest-palindromic-substring"
+# browser = driver
+driver.get(url)
+html = driver.page_source
+soup = BeautifulSoup(html, 'lxml')
+a = soup.findAll("div", {"class": "content__u3I1 question-content__JfgR"})
+print(a)
