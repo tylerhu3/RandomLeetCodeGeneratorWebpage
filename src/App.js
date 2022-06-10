d5df9632e68085694202365115bd3ae4a512744e
Tyler Hu <tylerhu@yahoo.com>
2022-06-09 21:44:41 -0700

    Adding dangerous logo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import jsonData from "./data2.json";
 import { Button } from "react-bootstrap";
 import Typing from "react-typing-animation";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
@@ -10,12 +11,52 @@ class App extends React.Component {
     medium: [],
     hard: [],
     theProblem: null,
+    showDangerModal: false,
+    fetchedImage: null,
   };
 
   // constructor() {
   //   super();
   //   // this.vantaRef = React.createRef();
   // }
+
+  getImagine = () => {
+    if (this.state.showDangerModal) {
+      this.setState({ showDangerModal: false });
+      return;
+    }
+
+    axios
+      .get(
+        "https://ei.phncdn.com/pics/logos/7381.png",
+        {
+          responseType: "arraybuffer",
+        }
+      )
+      .then((response) => {
+        let temp = Buffer.from(response.data, "binary").toString("base64");
+        console.log("printing pic: " + temp);
+        this.setState({ fetchedImage: temp, showDangerModal: true });
+      });
+  };
+
+  displayImage = () => {
+    console.log("execute displayImage");
+    if (this.state.showDangerModal) {
+      console.log("this.state.fetchedImage: " + this.state.fetchedImage);
+      return (
+        <div style={this.DangerModal}>
+          <img
+            id="imgElem"
+            alt="Danger Image"
+            src={"data:image/jpg;base64," + this.state.fetchedImage}
+          />
+        </div>
+      );
+    } else {
+      return <div style={this.DangerModal}>Nothing Here</div>;
+    }
+  };
 
   componentDidMount() {
     let easy = [];
@@ -50,18 +91,22 @@ class App extends React.Component {
 
   AnimatedTypingComponent = () => (
     <div
-      style={{ width: "100%",  textShadow: '4px 4px 4px #FFFFFF',
-      color: "black", fontSize: "290%" }}
+      style={{
+        width: "100%",
+        textShadow: "4px 4px 4px #FFFFFF",
+        color: "black",
+        fontSize: "290%",
+      }}
       onClick={() => this.openInNewTab(this.state.theProblem.url)}
     >
       <Typing
-    hideCursor={false}
+        hideCursor={false}
         key={new Date().getTime()} //force a component re-render
       >
-        <span >
+        <span>
           {" "}
           {this.state.theProblem
-            ? this.state.theProblem.title + "SSDS"
+            ? this.state.theProblem.title + "What"
             : ""}{" "}
         </span>
         <Typing.Backspace count={5} />
@@ -74,7 +119,6 @@ class App extends React.Component {
     let problem = this.state.theProblem;
     return (
       <div className={"App"}>
-
         <div className={"mainContent"}>
           {this.AnimatedTypingComponent()}
           <div style={this.contentStyle}>
@@ -116,6 +160,16 @@ class App extends React.Component {
             </Button>
           </div>
         </div>
+        {this.displayImage()}
+        <div>
+          <Button
+            style={this.DangerStyle}
+            variant="danger"
+            onClick={this.dangerResponse}
+          >
+            DANGER{" "}
+          </Button>
+        </div>
       </div>
     );
   }
@@ -139,12 +193,29 @@ class App extends React.Component {
     margin: "10px",
   };
 
+  DangerStyle = {
+    position: "relative",
+    right: -1000,
+    bottom: 150,
+    marginTop: "30px",
+    zIndex: "4",
+  };
+
+  DangerModal = {
+    fontSize: "24px",
+    color: "white",
+    position: "relative",
+    right: -1000,
+    bottom: 150,
+    marginTop: "30px",
+    zIndex: "4",
+  };
+
   divStyle = {
     position: "relative",
     zIndex: "1",
     left: 0,
   };
-
 
   getRandomInt = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
@@ -172,6 +243,34 @@ class App extends React.Component {
     let x = this.getRandomInt(this.state.hard.length);
     let prob = this.state.hard[x];
     this.setState({ theProblem: prob });
+  };
+
+  VideoModal = (props) => {
+    return (
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: "rgba(0,0,0,0.5)",
+        }}
+      >
+        <iframe
+          title={props.src}
+          allowFullScreen
+          frameBorder="0"
+          height="315px"
+          src={props.src}
+          width="560px"
+        />
+      </div>
+    );
+  };
+
+  dangerResponse = () => {
+    this.getImagine();
   };
 }
 
